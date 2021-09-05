4bdeb5486b31d8ee134c0e18a2eb3ab592dcddd9
youknowkeshi <76035490+youknowkeshi@users.noreply.github.com>
2021-09-05 23:05:14 +0700

    gg

--- src/components/SelectColorButton.tsx
+++ src/components/SelectColorButton.tsx
@@ -1,12 +1,23 @@
-import { Clear, randomCol } from "../stores/PixelPainterStore"
-
-const Utility = () => {
+import { PixelPainterStore, setSelectedCol } from "../stores/PixelPainterStore"
+type SelectColorButtonProps = {
+  color: string;
+}
+const SelectColorButton = ({ color }: SelectColorButtonProps) => {
+  const state = PixelPainterStore.useState();
+  //modify this function to highlight correctly
+  const computeRingSize = () => {
+      if(state.selectCol == color){
+        return "ring-8 ring-green-400"
+      }else{
+        return ""
+      } 
+  }
   return (
-    <div className="flex justify-center space-x-3">
-      <button className="w-36" onClick={()=>Clear()}>Clear</button>
-      <button className="w-36" onClick={()=>randomCol()}>Random color</button>
+    <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
+      style={{ backgroundColor: color }}
+      onClick={()=>setSelectedCol(color)}
+    >
     </div>
   )
 }
-
-export default Utility
+export default SelectColorButton
